12a15186bdf7aa37e6f803c9b88f00f80fe184fe
daniil <dancha241@gmail.com>
2020-09-05 18:38:11 +0300

    Редактирование настроек

--- lawyer/settings_rel.py
+++ lawyer/settings_rel.py
@@ -1,7 +1,3 @@
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
