2718806a783e1cd5ab40b78dae65cabb8ed8cb40
daniil <dancha241@gmail.com>
2020-09-05 15:22:44 +0300

    Разделение файлов настройки

---
+++ lawyer/settings_local.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lawyer',
+        'USER': 'postgres',
+        'PASSWORD': '03081998',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
---
+++ lawyer/settings_rel.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lawyer',
+        'USER': 'lawyer',
+        'PASSWORD': '03081998Dan',
+        'HOST': '194.67.90.19',
+        'PORT': '5432',
+    }
+}
