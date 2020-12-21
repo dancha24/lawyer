4b3e53007ec4e268c55ec14783f1d01d6f870c19
daniil <dancha241@gmail.com>
2020-12-22 03:03:19 +0700

    Добавление связанных дел в отображение Исполнителей. Добавление фильтров в таблицу испонителей.

--- performers/admin.py
+++ performers/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from .models import *
+from affairs.models import Affairs
+
+
+class PerformersInline(admin.StackedInline):
+    model = Affairs
+    extra = 0
+    # fk_name = 'customers'
+    show_change_link = True
+    # readonly_fields = ('name',)
+    # list_display = ('name',)
+    # fields = ('name',)
+    exclude = (
+        'name', 'prisealready', 'priseperformeralready', 'deal_status', 'prise_status',
+        'customers', 'jobcategories', 'prise', 'date_in', 'date_out', 'priseperformer', 'deal')
 
 
 @admin.register(JobCategories)
@@ -12,7 +26,28 @@ class UserAccessAdmin(admin.ModelAdmin):
 class UserAccessAdmin(admin.ModelAdmin):
     list_display = ('fio_min', 'all_sum', 'all_sum_already', 'all_debt')
     search_fields = ['name', 'surname', 'patronymic', ]
-    ordering = ('name',)
+    inlines = (PerformersInline,)
+
+    def changelist_view(self, request, extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['all_sum_all_performers'] = Performers.all_sum_all_performers()
+        extra_context['all_sum_already_all_performers'] = Performers.all_sum_already_all_performers()
+        extra_context['all_debt_all_performers'] = Performers.all_debt_all_performers()
+        # extra_context['deals_in_no_prise_count'] = Affairs.deals_in_no_prise_count()
+        # extra_context['deals_in_no_prise_sum'] = Affairs.deals_in_no_prise_sum()
+        return super().changelist_view(request, extra_context=extra_context)
+
+    # def change_view(self, request, object_id, form_url='', extra_context=None):
+    #     extra_context = extra_context or {}
+    #     extra_context['osm_data'] = Affairs.objects.get(pk=object_id)
+    #     return super(UserAccessAdmin, self).change_view(request, object_id,
+    #                                                     form_url, extra_context=extra_context)
+
+# @admin.register(Performers)
+# class UserAccessAdmin(admin.ModelAdmin):
+#     list_display = ('fio_min', 'all_sum', 'all_sum_already', 'all_debt')
+#     search_fields = ['name', 'surname', 'patronymic', ]
+#     ordering = ('name',)
 
     # def change_view(self, request, object_id, form_url='', extra_context=None):
     #     extra_context = extra_context or {}
