d4494eedd1df283577469128d809f1755263a21e
daniil <dancha241@gmail.com>
2021-01-22 14:02:20 +0700

    Исполнители
    Редактирование

--- performers/views.py
+++ performers/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, permission_required
 from .models import Performers, PerformersDoc
+from finansy.models import Spending
 
 
 # Список всех Исаолнителей
@@ -21,13 +22,15 @@ def performers_all(request):
 @permission_required('performers.view_performers', raise_exception=True)  # Проверка прав
 def performers_info(request, performers_id):
     performers = Performers.objects.get(pk=performers_id)
+    spending = Spending.objects.filter(performers_id=performers_id)
     context = {
         'info': performers,
+        'spending': spending,
         # 'extra_affairs': extra_affairs,
         # 'performers': performers_with_prise,
         'menu': 'performers',
         'submenu': 'performers_all',
-        'titlepage': 'Информация о деле ' + performers.name,
+        'titlepage': 'Информация о деле ' + str(performers),
     }
 
-    return render(request, 'affairs/affairs_info.html', context)
+    return render(request, 'performers/performers_info.html', context)
