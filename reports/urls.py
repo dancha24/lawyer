168f6b4c12a7250908c0e4fd864890c05b3ec429
daniil <dancha241@gmail.com>
2021-08-04 17:56:58 +0700

    Исправление отчета по выплатам исполнителям за период

--- reports/urls.py
+++ reports/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     # отчеты
     path('all', views.reports_all, name='reports_all'),
     path('glav_law_report', views.report_glav_law, name='report_glav_law'),
-    path('report_ispolnitel', views.report_nagrada_ispolnitel_data, name='report_nagrada_ispolnitel_data'),
+    path('report_nagrada_ispolnitel_data', views.report_nagrada_ispolnitel_data, name='report_nagrada_ispolnitel_data'),
     path('ans/report_glav_law/<date_in>/<date_in_max>/<performer_id>', views.report_glav_law_ans, name='report_glav_law_ans'),
-    path('ans/report_ispolnitel/<date_in>/<date_in_max>', views.report_nagrada_ispolnitel_data_ans, name='report_nagrada_ispolnitel_data_ans'),
+    path('ans/report_nagrada_ispolnitel_data/<date_in>/<date_in_max>', views.report_nagrada_ispolnitel_data_ans, name='report_nagrada_ispolnitel_data_ans'),
 
 ]
