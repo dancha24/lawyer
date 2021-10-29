f32ec966fef8e0d104dc0fd8c902b8e052a340d8
daniil <dancha241@gmail.com>
2021-10-29 12:04:08 +0700

    Настройки бд вынесены

--- lawyer/settings_local.py
+++ lawyer/settings_local.py
@@ -87,14 +87,3 @@ STATIC_ROOT = 'static/'
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'lawyer',
-        'USER': 'postgres',
-        'PASSWORD': '03081998',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
--- lawyer/settings_rel.py
+++ lawyer/settings_rel.py
@@ -87,17 +87,3 @@ USE_TZ = True
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'lawyer',
-        'USER': 'lawyer',
-        'PASSWORD': '03081998Dan',
-        'HOST': '178.21.8.132',
-        # 'USER': 'postgres',
-        # 'PASSWORD': '03081998',
-        # 'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
