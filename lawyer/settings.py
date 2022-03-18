54284cfd6f3c22d640476aa2b5775e38bb67ba30
daniil <dancha241@gmail.com>
2022-03-19 00:18:00 +0700

    Изменение бд

--- lawyer/settings.py
+++ lawyer/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 from lawyer.settings_rel import *
-from lawyer.bd import *
+from settings.bd import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
---
+++ settings/bd.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lawyer',
+        'USER': 'dancha',
+        'PASSWORD': '03081998Dan',
+        'HOST': '89.108.78.140',
+        'PORT': '5432',
+    }
+}
