d23585659d8c9b9d8e3b096cd452b78f0ea7189a
Dancha <dancha241@gmail.com>
2023-02-01 21:51:53 +0700

    Подготовка селдерея для работы

---
+++ lawyer/celery.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+import os
+from celery import Celery
+
+# этот код скопирован с manage.py
+# он установит модуль настроек по умолчанию Django для приложения 'celery'.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lawyer.settings')
+
+# здесь вы меняете имя
+app = Celery("lawyer")
+
+# Для получения настроек Django, связываем префикс "CELERY" с настройкой celery
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# загрузка tasks.py в приложение django
+app.autodiscover_tasks()
+
+
+@app.task
+def add(x, y):
+    return x / y
--- lawyer/settings.py
+++ lawyer/settings.py
@@ -45,3 +45,6 @@ TEMPLATES = [
         },
     },
 ]
+
+CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
+CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
